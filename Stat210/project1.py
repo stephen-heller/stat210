8e34ae7e2f631564b46f8c72b462d606df0d9e62
Stephen Heller <stephenheller334@gmail.com>
2026-04-01 23:25:41 -0500

    Added Benford's law distribution, removed trash files

--- Stat210/project1.py
+++ Stat210/project1.py
@@ -1,18 +1,23 @@
 from pybaseball import batting_stats
 from matplotlib import pyplot as plt
+import math
 
-leading_digits = 9 * [0]
+leading_digit_quantities = 9 * [0]
 
 data = batting_stats(2015, 2025, ind=0)
 print(max(data["HR"]))
+print(min(data["HR"]))
+print(len(data["HR"]))
 for num in data["HR"]:
     first_digit = int(str(num)[0])
-    leading_digits[first_digit - 1] += 1
+    leading_digit_quantities[first_digit - 1] += 1
 
-print(leading_digits)
+print(leading_digit_quantities)
 
-
-fig = plt.bar([1, 2, 3, 4, 5, 6, 7, 8, 9], leading_digits,)
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+benford_probs = [len(data["HR"])*math.log10(1 + 1 / digit) for digit in digits]
+fig = plt.bar(digits, leading_digit_quantities,)
+plt.plot(digits, benford_probs, color='red', marker='o',)
 plt.xlabel("Leading digit of home run count from 2015-2025")
 plt.ylabel("Frequency")
-plt.savefig("test2.png")
+plt.savefig("distribution.png")
